69575bac4d0ff4d192dbc83ff518eb5c83fd4c0e
Рацкин Расим Александрович <rasim-82@mail.ru>
2018-02-27 03:35:36 +0300

    tu

--- MasterNodesManager/Program.cs
+++ MasterNodesManager/Program.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using System.Threading;
 
 namespace MasterNodesManager
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            //_settings = new Settings();
+
+
+            Console.WriteLine("Загрузка настроек");
+
+
+            Console.WriteLine("Загрузка истории");
+
             Console.WriteLine("Менеджер Мастернод начал арбайтен прсторонись =)))))");
             Console.WriteLine("----------------------------------------------------");
             Console.ReadLine();
+            Console.WriteLine("Сохранение настроек");
+            Console.WriteLine("Сохранение истории");
+            Thread.Sleep(5000);
         }
     }
 }
